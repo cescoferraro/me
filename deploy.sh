5ed6a7092bf58eb314074f456f5eade99cf6770a
cesco <francescoaferraro@gmail.com>
2017-09-23 17:47:15 -0300

    Bug fix

--- deploy.sh
+++ deploy.sh
@@ -61,7 +61,7 @@ echo 555555
 ls 
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
