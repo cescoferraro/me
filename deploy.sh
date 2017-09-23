ec5add4d33211fb6a568900f436132d26b8764c3
cescoferraro <francescoaferraro@gmail.com>
2017-09-22 21:56:50 -0300

    push

--- deploy.sh
+++ deploy.sh
@@ -55,6 +55,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../travis.enc -out ../travis -d
+ls ../
 chmod 600 ../travis
 
 eval `ssh-agent -s`
